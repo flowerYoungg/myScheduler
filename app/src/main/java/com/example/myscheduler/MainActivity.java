0c09114926ac908392dd7fa7bcdb2b30966334d4
flowerYoungg <khy1896@gamil.com>
2020-07-05 22:52:17 +0900

    Login Display

--- app/src/main/java/com/example/myscheduler/MainActivity.java
+++ app/src/main/java/com/example/myscheduler/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.myscheduler;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.LoginButton);
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                // 인텐트 객체 생성
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                // 새로운 액티비티 시작
+                startActivity(intent);
+            }
+        });
     }
 }
